d61a807f839e1f8a9319769845bb328ab1e5c347
Ankit Guria <39993421+ankit142@users.noreply.github.com>
2023-08-31 01:28:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,30 +6,43 @@ const saveButton = document.getElementById('save-button');
 saveButton.addEventListener('click', () => {
     const userInput = editor.value;
 
-    // Send a POST request to save the user input on the server
-    fetch('/.netlify/functions/save', { // Update the path to your save function
-        method: 'POST',
-        body: `userInput=${userInput}`,
+    // Send a PUT request to save the user input on GitHub
+    const githubUsername = 'ankit142'; // Replace with your GitHub username
+    const repoName = 'ankit142.github.io'; // Replace with your repository name
+    const token = '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'; // Replace with your personal access token
+    const apiUrl = `https://api.github.com/repos/${githubUsername}/${repoName}/contents/note.txt`;
+
+    fetch(apiUrl, {
+        method: 'PUT',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `token ${token}`,
+            'Accept': 'application/vnd.github.v3+json',
         },
+        body: JSON.stringify({
+            message: 'Update note.txt', // Commit message
+            content: btoa(userInput), // Encode text to base64
+            sha: 'sha-of-existing-file', // Get the SHA of the existing file from GitHub or create a new one
+        }),
     })
         .then((response) => response.text())
-        .then((data) => {
-            console.log(data); // Log the server's response
+        .then(() => {
+            console.log('Text saved successfully');
         })
         .catch((error) => {
             console.error('Error saving user input:', error);
         });
 });
 
-// Function to load and display saved input from the server when the page loads
+// Function to load and display saved input from GitHub when the page loads
 window.addEventListener('load', () => {
-    // Send a GET request to load the saved user input from the server
-    fetch('/.netlify/functions/load') // Update the path to your load function
-        .then((response) => response.text())
+    // Send a GET request to load the saved user input from GitHub
+    const githubUrl = `https://api.github.com/repos/${githubUsername}/${repoName}/contents/note.txt`;
+
+    fetch(githubUrl)
+        .then((response) => response.json())
         .then((data) => {
-            editor.value = data;
+            const decodedContent = atob(data.content); // Decode base64 content
+            editor.value = decodedContent;
         })
         .catch((error) => {
             console.error('Error loading user input:', error);
