19c4ec498e7452a6549fab5d37f9bf7b0416cd3b
Ankit Guria <39993421+ankit142@users.noreply.github.com>
2023-08-31 12:20:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,33 +4,42 @@ const saveButton = document.getElementById('save-button');
 
 // Event listener for the Save button
 saveButton.addEventListener('click', () => {
-    /* const userInput = editor.value;
+    updateFile();
+const userInput = editor.value;
+const encodedInput = btoa(userInput);
+const token = "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"; // Replace with your personal access token
+const apiUrl = "https://api.github.com/repos/ankit142/ankit142.github.io/contents/note.txt";
 
-    // Send a PUT request to save the user input on GitHub
-    const githubUsername = 'ankit142'; // Replace with your GitHub username
-    const repoName = 'ankit142.github.io'; // Replace with your repository name
-    const token = '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'; // Replace with your personal access token
-    const apiUrl = 'https://api.github.com/repos/ankit142/ankit142.github.io/contents/note.txt';
+const updateFile = async () => {
+    const content = "SGVsbG8KYWJk"; // Content to update, in base64 format
+    const sha = "e965047ad7c57865823c7d992b1d046ea66edf78"; // SHA of the existing file
 
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Authorization': `token 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`,
-            'Accept': 'application/vnd.github.v3+json',
-        },
-        body: JSON.stringify({
-            message: 'Update note.txt', // Commit message
-            content: btoa(userInput), // Encode text to base64
-            sha: 'https://github.com/ankit142/ankit142.github.io/raw/main/note.txt', // Get the SHA of the existing file from GitHub or create a new one
-        }),
-    })
-        .then((response) => response.text())
-        .then(() => {
-            console.log('Text saved successfully');
-        })
-        .catch((error) => {
-            console.error('Error saving user input:', error);
-        }); */
+    const requestBody = {
+        message: 'Update note.txt', // Commit message
+        content: encodedInput, // Content in base64
+        sha: sha // SHA of the existing file
+    };
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `token ${token}`,
+                'Accept': "application/vnd.github.v3+json",
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        if (response.ok) {
+            console.log('File updated successfully');
+        } else {
+            console.error('Failed to update file:', response.status, response.statusText);
+        }
+    } catch (error) {
+        console.error('Error updating file:', error);
+    }
+};
     // Initialize GitHub with your personal access token
 /* const github = new GitHub({
     token: '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', // Replace with your personal access token
@@ -60,7 +69,7 @@ repo.getContents('main', 'note.txt', true) // 'main' is the branch name
     .catch((error) => {
         console.error('Error saving user input:', error);
     }); */
-const githubUsername = 'ankit142'; // Replace with your GitHub username
+/* const githubUsername = 'ankit142'; // Replace with your GitHub username
     const repoName = 'ankit142.github.io'; // Replace with your repository name
     const token = "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"; 
     
@@ -106,7 +115,7 @@ fetch(apiUrl)
   .catch((error) => {
     console.error('Error retrieving existing file content:', error);
   });
-
+ */
 });
 
 // Function to load and display saved input from GitHub when the page loads
